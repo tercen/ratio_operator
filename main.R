c114693c86e8770b0db617653f6e647db7c9e6d2
agouy <alexandre.gouy@tercen.com>
2024-11-12 14:50:11 +0000

    Added method setting + fixed typo

--- main.R
+++ main.R
@@ -1,17 +1,26 @@
 library(tercen)
 library(dplyr)
+library(tidyr)
 
 ctx = tercenCtx()
 
 dividend_col_id <- ctx$op.value(name = "Dividend Column ID", as.double, 0)
 divisor_col_id <- ctx$op.value(name = "Divisor Column ID", as.double, 0)
+mode <- ctx$op.value(name = "Method", as.character, "y / x")
 
 if(dividend_col_id > 0 & divisor_col_id > 0) {
   
   ctx %>% 
     select(.ri, .ci, .y) %>%
+    tidyr::complete(.ri, .ci) %>%
+    arrange(.ri, .ci) %>%
     group_by(.ri) %>%
-    summarise(ratio = .y[.ci == dividend_col_id - 1L] / .y[.ci == divisor_col_id- 1L])
+    summarise(ratio = if_else(
+      mode == "y / x",
+      .y[.ci == dividend_col_id - 1L] / .y[.ci == divisor_col_id- 1L],
+      .y[.ci == dividend_col_id - 1L] / (.y[.ci == dividend_col_id - 1L] + .y[.ci == divisor_col_id- 1L]),
+    )) %>%
+    tidyr::drop_na() %>%
     ctx$addNamespace() %>%
     ctx$save()
     
@@ -20,7 +29,11 @@ if(dividend_col_id > 0 & divisor_col_id > 0) {
   ctx %>% 
     select(.ri, .ci, .x, .y) %>% 
     group_by(.ri, .ci) %>%
-    transmute(ratio = .y / .x) %>%
+    transmute(ratio = if_else(
+      mode == "y / x",
+      .y / .x,
+      .y / (.x + .y)
+    )) %>%
     ctx$addNamespace() %>%
     ctx$save()
   
