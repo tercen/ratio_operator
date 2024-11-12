b83a1a99f9a5884b3ba7a452d09dadd680acc689
agouy <alexandre.gouy@tercen.com>
2024-11-12 10:03:19 +0000

    feat: Support for division between columns

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ratio operator
 
 ##### Description
+
 `ratio` operator computes the ratio of two data points.
 
 ##### Usage
@@ -15,14 +16,5 @@ Output relations|.
 `ratio`          | numeric, ratio value returned per cell
 
 ##### Details
+
 The operator takes the value defined by the y-axis and divides it by the value defined by the x-axis. The computation is done per cell. There is one value calculated and returned for each data point.
-
-#### References
-
-
-##### See Also
-
-[sum](https://github.com/tercen/sum_operator), [product](https://github.com/tercen/product_operator)
-
-
-#### Examples
--- main.R
+++ main.R
@@ -1,12 +1,28 @@
 library(tercen)
 library(dplyr)
 
-
 ctx = tercenCtx()
 
-ctx %>% 
-  select(.ri, .ci, .x, .y) %>% 
-  group_by(.ri,.ci) %>%
-  transmute(ratio = .y/.x) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+dividend_col_id <- ctx$op.value(name = "Dividend Column ID", as.double, 0)
+divisor_col_id <- ctx$op.value(name = "Divisor Column ID", as.double, 0)
+
+if(dividend_col_id > 0 & divisor_col_id > 0) {
+  
+  ctx %>% 
+    select(.ri, .ci, .y) %>%
+    group_by(.ri) %>%
+    summarise(ratio = .y[.ci == dividend_col_id - 1L] / .y[.ci == divisor_col_id- 1L])
+    ctx$addNamespace() %>%
+    ctx$save()
+    
+} else {
+  
+  ctx %>% 
+    select(.ri, .ci, .x, .y) %>% 
+    group_by(.ri, .ci) %>%
+    transmute(ratio = .y / .x) %>%
+    ctx$addNamespace() %>%
+    ctx$save()
+  
+}
+
